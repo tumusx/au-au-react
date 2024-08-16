b3b907b7bc96e4adf09305d0524056bd7dbb712f
Murillo <tumusx1@gmail.com>
2024-08-15 22:44:36 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TextField } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextField color='warning' id="outlined-basic" label="CheckBox Color" variant="outlined" sx={{'& .MuiInputBase-input': {
+          color: 'white', 
+        },}}
+  />        </p>
       </header>
     </div>
   );
